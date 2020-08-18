948c0cdd649fc77fcd3b047b5148ac9f07469e6a
Jan <j.cerny.zdar@gmail.com>
2020-08-18 21:03:44 +0200

    add tests

---
+++ firmware/tests/magnet-test/magnet-test.ino
@@ -0,0 +1,12 @@
+int magnet_pin = 8;
+
+void setup() {
+  pinMode(mahnet_pin, OUTPUT);
+
+}
+void loop() {
+ digitalWrite(magnet_pin, HIGH);
+ delay(15000);
+ digitalWrite(magnet_pin, LOW);
+ delay(15000);
+} 
